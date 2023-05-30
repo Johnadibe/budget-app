1cb45c3dd80893be066c907b96c156bd7979da97
John Adibe <johnadibe123@gmail.com>
2023-05-29 17:03:51 -0800

    Fix rubocop errors

--- app/models/money_group.rb
+++ app/models/money_group.rb
@@ -1,17 +1,17 @@
 class MoneyGroup < ApplicationRecord
-    belongs_to :group
-    belongs_to :operation
+  belongs_to :group
+  belongs_to :operation
 
-    after_create :increment_amount
-    before_destroy :decrement_amount
+  after_create :increment_amount
+  before_destroy :decrement_amount
 
-    def increment_amount
-        current_amount = group.total_amount
-        group.update(total_amount: current_amount + operation.amount)
-      end
-    
-      def decrement_amount
-        subtracted_amount = group.total_amount - operation.amount
-        group.update(total_amount: subtracted_amount)
-      end
+  def increment_amount
+    current_amount = group.total_amount
+    group.update(total_amount: current_amount + operation.amount)
+  end
+
+  def decrement_amount
+    subtracted_amount = group.total_amount - operation.amount
+    group.update(total_amount: subtracted_amount)
+  end
 end
--- test/models/money_group_test.rb
+++ test/models/money_group_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class MoneyGroupTest < ActiveSupport::TestCase
   # test "the truth" do
