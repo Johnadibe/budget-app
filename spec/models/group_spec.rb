aed49a721e30eb55f728c3f9c4587e795854a50a
John Adibe <johnadibe123@gmail.com>
2023-05-31 14:56:17 -0800

    Add test files

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'John Adibe', email: 'example@mail.com', password: 'password') }
+  let(:group) { Group.create(name: 'Bag', icon: 'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=600', user_id: user.id) }
+
+  describe 'Validations' do
+    context 'when valid' do
+      it { expect(group).to be_valid }
+    end
+
+    it 'should allow valid name' do
+      group.name = 'Eg'
+      expect(group).to_not be_valid
+    end
+
+    it 'should allow valid name' do
+      group.name = 'Outing'
+      expect(group).to be_valid
+    end
+
+    it 'should allow valid user_id' do
+      group.user_id = nil
+      expect(group).to_not be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'John Adibe', email: 'example@mail.com', password: 'password')
+  end
+  before { subject.save }
+
+  it 'name should not be nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should valid the name' do
+    subject.name = 'John Adibe'
+    expect(subject).to be_valid
+  end
 end
