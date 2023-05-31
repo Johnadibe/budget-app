4809b1c822f5979f374257169b99396e98599804
John Adibe <johnadibe123@gmail.com>
2023-05-31 10:36:08 -0800

    Add total_amount column to Group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_183542) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_174722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_183542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "total_amount", default: 0
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
