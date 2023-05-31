f6ce30cde09f9ba17391d46a2da624e2c035d635
John Adibe <johnadibe123@gmail.com>
2023-05-30 16:37:31 -0800

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email, password, name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password name])
   end
 end
